385ba338931aafc67b7ba34f73378162974efa8f
Exfemida <Exfemida@gmail.com>
2021-01-31 00:04:50 +0300

    Функция вычисления расстояния между точками
    задание 4

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,12 +2,13 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello, Im Luntik!");
-
 		Point p1=new Point(2,2);
 		Point p2=new Point(4,4);
 
-		System.out.println(distance(p1,p2));
+		System.out.println("Расстояние между точками с координатами: ");
+		System.out.println("x="+p1.x+" ,y="+p1.y);
+		System.out.println("x="+p2.x+" ,y="+p2.y);
+		System.out.println("составляет "+distance(p1,p2));
 	}
 	public static double distance(Point p1, Point p2) {
 		double l;
