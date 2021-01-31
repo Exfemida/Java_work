16b7a3d0ba0b074b2e89acea668414f9214b95d4
Exfemida <Exfemida@gmail.com>
2021-01-31 15:21:48 +0300

    функция перенесена в класс

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,17 +2,12 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		Point p1=new Point(2,2);
-		Point p2=new Point(4,4);
+		Point p=new Point(2,2,4,4);
+
 
 		System.out.println("Расстояние между точками с координатами: ");
-		System.out.println("x="+p1.x+" ,y="+p1.y);
-		System.out.println("x="+p2.x+" ,y="+p2.y);
-		System.out.println("составляет "+distance(p1,p2));
-	}
-	public static double distance(Point p1, Point p2) {
-		double l;
-		l=Math.sqrt((p2.x- p1.x)*(p2.x- p1.x)+(p2.y- p1.y)*(p2.y- p1.y));
-		return l;
+		System.out.println("x="+p.x1+" ,y="+p.y1);
+		System.out.println("x="+p.x2+" ,y="+p.y2);
+		System.out.println("составляет "+p.distance());
 	}
 }
