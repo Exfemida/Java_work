7575950014499215cbd633a86f695ab04cab2e1b
Exfemida <Exfemida@gmail.com>
2021-02-07 00:08:38 +0300

    Конец второй части теории лекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -25,17 +24,17 @@ public class ApplicationManager {
   }
 
 
-<<<<<<< HEAD
-  public void login(String username, String password) {
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
-=======
->>>>>>> parent of 28b47e6... Revert "Добавили NavigatorHelper"
+//<<<<<<< HEAD
+//  public void login(String username, String password) {
+//    wd.findElement(By.name("user")).clear();
+//    wd.findElement(By.name("user")).sendKeys(username);
+//    wd.findElement(By.name("pass")).click();
+//    wd.findElement(By.name("pass")).clear();
+//   wd.findElement(By.name("pass")).sendKeys(password);
+//    wd.findElement(By.xpath("//input[@value='Login']")).click();
+//  }
+//=======
+//>>>>>>> parent of 28b47e6... Revert "Добавили NavigatorHelper"
 
   public void stop() {
     wd.findElement(By.linkText("Logout")).click();
@@ -51,14 +50,7 @@ public class ApplicationManager {
     }
   }
 
-  public boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+
 
   public GroupHelper getGroupHelper() {
     return groupHelper;
