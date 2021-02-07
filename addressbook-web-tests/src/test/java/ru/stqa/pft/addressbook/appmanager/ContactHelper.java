5733c6c029a9c1a4daef6704b7e8739628fe6453
Exfemida <Exfemida@gmail.com>
2021-02-07 18:26:17 +0300

    Добавлен метод ContactModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -54,4 +54,40 @@ public class ContactHelper extends HelperBase {
   public void closeAlertDelete() {
     wd.switchTo().alert().accept();
   }
+
+  public void editContact() {
+    click(By.xpath("(//img[@alt='Edit'])[3]"));     //click(By.xpath("(//img[@alt='Edit'])["+numStr+"]"));
+  }
+
+  public void submitContactUpdate() {  //String numStr
+    click(By.name("update"));
+  }
+  public void editContactForm(Contacts contacts) {
+    type(By.name("firstname"),contacts.getFirstname());
+    type(By.name("middlename"),contacts.getMiddlename());
+    type(By.name("lastname"),contacts.getLastname());
+    type(By.name("nickname"),contacts.getNickname());
+    type(By.name("title"),contacts.getTitle());
+    type(By.name("company"),contacts.getCompany());
+    type(By.name("address"),contacts.getAddress());
+    type(By.name("home"),contacts.getHome());
+    type(By.name("mobile"),contacts.getMobile());
+    type(By.name("work"),contacts.getWork());
+    type(By.name("fax"),contacts.getFax());
+    type(By.name("email"),contacts.getEmail());
+    type(By.name("email2"),contacts.getEmail2());
+    type(By.name("email3"),contacts.getEmail3());
+    type(By.name("homepage"),contacts.getHomepage());
+    select(By.name("bday"), contacts.getBday());
+    select(By.name("bmonth"), contacts.getBmonth());
+    type(By.name("byear"),contacts.getByear());
+    select(By.name("aday"), contacts.getAday());
+    select(By.name("amonth"), contacts.getAmonth());
+    type(By.name("ayear"),contacts.getAyear());
+
+    type(By.name("address2"),contacts.getAddress2());
+    type(By.name("phone2"),contacts.getPhone2());
+    type(By.name("notes"),contacts.getNotes());
+  }
+
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.Contacts;
+
+public class ContactModificationTests extends TestBase{
+
+  @Test
+  public void testContactModification (){
+    String numStr="2";
+    app.getContactHelper().editContact();  //numStr
+    app.getContactHelper().editContactForm(new Contacts("Maria", "Sergeevna", "Ivanova", "Mashka", "do not know", "Rosneft", "Moskwa, 6", "2222222", "3333333", "4444444", "5555555", "email_1", "email_2", "email_3", "mashka.ru", "8", "May", "1982", "12", "September", "2004", "test1", "Kiev22", "3422", "kak dela?22"));
+    app.getContactHelper().submitContactUpdate(); // numStr
+    app.getNavigationHelper().gotoStartPage();
+
+  }
+
+}
