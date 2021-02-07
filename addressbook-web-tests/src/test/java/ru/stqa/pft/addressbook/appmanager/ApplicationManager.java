d527426e7ec758ae45fab4268dd3b4b9542fc37f
Exfemida <Exfemida@gmail.com>
2021-02-07 14:21:35 +0300

    добавлен метод select

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -44,7 +44,6 @@ public class ApplicationManager {
   public GroupHelper getGroupHelper() {
     return groupHelper;
   }
-
   public NavigationHelper getNavigationHelper() {
     return navigationHelper;
   }
