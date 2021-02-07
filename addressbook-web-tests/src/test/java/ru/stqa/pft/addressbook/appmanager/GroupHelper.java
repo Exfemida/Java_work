a5b13f614ed84d4c953323f25058a4602eebbf40
Exfemida <Exfemida@gmail.com>
2021-02-07 15:50:56 +0300

    Добавлен класс GroupModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -30,10 +30,18 @@ public class GroupHelper extends HelperBase {
   }
 
   public void deleteSelectedGroups() {
-    click(By.xpath("(//input[@name='delete'])[2]"));
+    click(By.name("delete"));
   }
 
   public void selectGroup() {
     click(By.name("selected[]"));
+  }
+
+  public void initGroupModification() {
+    click(By.name("edit"));
+  }
+
+  public void submitGroupModification() {
+    click(By.name("update"));
   }
 }
