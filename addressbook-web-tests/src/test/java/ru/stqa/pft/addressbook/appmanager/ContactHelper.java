c7368d1da0507070876f7a140bd70ecc20eff42e
Exfemida <Exfemida@gmail.com>
2021-02-13 13:54:54 +0300

    Удален неактуальный метод editContactForm

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -45,8 +45,6 @@ public class ContactHelper extends HelperBase {
     } else {
       Assert.assertFalse(isElementPresent(By.name("new_group")));
     }
-
-    //select(By.name("new_group"), contacts.getNewGroup());
     type(By.name("address2"),contacts.getAddress2());
     type(By.name("phone2"),contacts.getPhone2());
     type(By.name("notes"),contacts.getNotes());
@@ -71,32 +69,4 @@ public class ContactHelper extends HelperBase {
   public void submitContactUpdate() {
     click(By.name("update"));
   }
-  public void editContactForm(Contacts contacts) {
-    type(By.name("firstname"),contacts.getFirstname());
-    type(By.name("middlename"),contacts.getMiddlename());
-    type(By.name("lastname"),contacts.getLastname());
-    type(By.name("nickname"),contacts.getNickname());
-    type(By.name("title"),contacts.getTitle());
-    type(By.name("company"),contacts.getCompany());
-    type(By.name("address"),contacts.getAddress());
-    type(By.name("home"),contacts.getHome());
-    type(By.name("mobile"),contacts.getMobile());
-    type(By.name("work"),contacts.getWork());
-    type(By.name("fax"),contacts.getFax());
-    type(By.name("email"),contacts.getEmail());
-    type(By.name("email2"),contacts.getEmail2());
-    type(By.name("email3"),contacts.getEmail3());
-    type(By.name("homepage"),contacts.getHomepage());
-    select(By.name("bday"), contacts.getBday());
-    select(By.name("bmonth"), contacts.getBmonth());
-    type(By.name("byear"),contacts.getByear());
-    select(By.name("aday"), contacts.getAday());
-    select(By.name("amonth"), contacts.getAmonth());
-    type(By.name("ayear"),contacts.getAyear());
-
-    type(By.name("address2"),contacts.getAddress2());
-    type(By.name("phone2"),contacts.getPhone2());
-    type(By.name("notes"),contacts.getNotes());
-  }
-
 }
