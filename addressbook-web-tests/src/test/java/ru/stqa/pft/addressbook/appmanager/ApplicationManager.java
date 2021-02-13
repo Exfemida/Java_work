8cacc6de982f9da638fdd0e076dbad5df69376cd
Exfemida <Exfemida@gmail.com>
2021-02-13 11:21:46 +0300

    == заменено на equals

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -23,9 +23,9 @@ public class ApplicationManager {
   }
 
   public void init() {
-    if (browser== BrowserType.CHROME){
+    if (browser.equals(BrowserType.CHROME)){
       wd = new ChromeDriver();
-    } else if (browser==BrowserType.FIREFOX){
+    } else if (browser.equals(BrowserType.FIREFOX)){
       wd = new FirefoxDriver();}
      // else if (browser==BrowserType.EDGE){     Вызывается старая версия IE, драйвер не подходит
      // }
