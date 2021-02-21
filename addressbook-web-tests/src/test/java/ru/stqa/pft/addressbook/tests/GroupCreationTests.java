35fb7221bcebc1cae62087e4fae87c100d168484
Exfemida <Exfemida@gmail.com>
2021-02-21 16:23:08 +0300

    переменные before и after заменены на списки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -15,6 +15,5 @@ public class GroupCreationTests extends TestBase {
     app.getGroupHelper().createGroup(new GroupDate("test1", null, null));
     List<GroupDate> after=app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size()+1);
-
   }
 }
