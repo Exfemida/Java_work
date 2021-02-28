1386ae1490bcf438b48e3fd301ae98d1a402b247
Exfemida <Exfemida@gmail.com>
2021-02-28 21:56:54 +0300

    удаление без проверки с созданием - готово

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -14,28 +14,25 @@ public class ContactDeletionTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
+    if (app.contact().all().size()==0) {          //проверка наличия в базе созданных контактов
+      app.goTo().groupPage();                    //проверка наличия в базе созданных групп
+      if (app.group().all().size()==0) {
+        app.group().create(new GroupDate().withName("test2"));
+      }
+      app.goTo().AddContactPage();
+      app.contact().create(new ContactDate()
+              .withFirstname("Maria").withMiddlename("Sergeevna").withLastname("Ivanova")
+              .withNickname("Mashka").withTitle("do not know").withCompany("Rosneft")
+              .withAddress("Moskwa, 6").withHome("2222222").withMobile("3333333").withWork("4444444")
+              .withFax("5555555").withEmail("email_1").withEmail2("email_2").withEmail3("email_3")
+              .withHomepage("mashka.ru").withBday("8").withBmonth("May").withByear("1982")
+              .withAday("12").withAmonth("September").withAyear("2004")
+              .withNewGroup("test1").withAddress2("Kiev").withPhone2("34").withNotes("kak dela?"), true);
+    }
   }
 
   @Test (enabled = true)
   public void testContactDeletion() {
-
-//    if (app.contact().all().size()==0) {          //проверка наличия в базе созданных контактов
-//      app.goTo().groupPage();                    //проверка наличия в базе созданных групп
-//      if (app.group().all().size()==0) {
-//        app.group().create(new GroupDate().withName("test2"));
-//      }
-//      app.goTo().AddContactPage();
-//      app.contact().create(new ContactDate()
-//              .withFirstname("Maria").withMiddlename("Sergeevna").withLastname("Ivanova")
-//              .withNickname("Mashka").withTitle("do not know").withCompany("Rosneft")
-//              .withAddress("Moskwa, 6").withHome("2222222").withMobile("3333333").withWork("4444444")
-//              .withFax("5555555").withEmail("email_1").withEmail2("email_2").withEmail3("email_3")
-//              .withHomepage("mashka.ru").withBday("8").withBmonth("May").withByear("1982")
-//              .withAday("12").withAmonth("September").withAyear("2004")
-//              .withNewGroup("test1").withAddress2("Kiev").withPhone2("34").withNotes("kak dela?"), true);
-//    }
-
-
     app.goTo().StartPage();
     Contacts before=app.contact().all();
     ContactDate deletedContact = before.iterator().next();
