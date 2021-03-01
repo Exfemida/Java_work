0c5c93faf19f12322a00513e22a7586f76328171
Exfemida <Exfemida@gmail.com>
2021-03-01 23:44:55 +0300

    гоу некст

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTest.java
@@ -5,7 +5,7 @@ import ru.stqa.pft.addressbook.model.ContactDate;
 public class ContactPhoneTest extends TestBase {
   public void testContactPhones(){
     app.goTo().StartPage();
-    ContactDate contact =app.contact().iterator().next();
+    ContactDate contact =app.contact().all().iterator().next();
     ContactDate contactInfoFromEditForm = app.contact().infoFormEditForm(contact);
   }
 }
