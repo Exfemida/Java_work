525c2987ce98b269ca63c504221b6c18d3bae76f
Exfemida <Exfemida@gmail.com>
2021-03-13 13:46:50 +0300

    Update ContactCreationTest.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -50,15 +50,6 @@ public class ContactCreationTest extends TestBase {
     app.goTo().AddContactPage();
     File photo = new File("src/test/resources/stru.png");
     cont.withPhoto(photo);
-//    ContactDate cont = new ContactDate()
-//            .withFirstname("Maria").withMiddlename("Sergeevna").withLastname("Ivanova")
-//            .withNickname("Mashka").withTitle("do not know").withCompany("Rosneft")
-//            .withAddress("Moskwa, 6").withHomePhone("2222222").withMobilePhone("3333333").withWorkPhone("4444444")
-//            .withFax("5555555").withEmail("email_1").withEmail2("email_2").withEmail3("email_3")
-//            .withHomepage("mashka.ru").withBday("8").withBmonth("May").withByear("1982")
-//            .withAday("12").withAmonth("September").withAyear("2004")
-//            .withNewGroup("test1").withAddress2("Kiev").withPhone2("34").withNotes("kak dela?").withPhoto(photo);
-
     app.contact().create(cont, true);
     app.goTo().StartPage();
     Contacts after = app.contact().all();
