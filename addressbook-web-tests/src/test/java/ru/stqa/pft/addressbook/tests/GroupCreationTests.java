f6606d35e89be1e99301017b87645a7ce28e0e59
Exfemida <Exfemida@gmail.com>
2021-03-13 23:51:15 +0300

    урок11

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupDate;
@@ -17,6 +19,8 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
+
 
   @DataProvider
   public Iterator<Object[]> validGroupsFromXml() throws IOException {
@@ -51,6 +55,8 @@ public class GroupCreationTests extends TestBase {
 
   @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupDate group) {
+
+
     app.goTo().groupPage();
     Groups before=app.group().all();
     app.group().create(group);
@@ -59,6 +65,7 @@ public class GroupCreationTests extends TestBase {
     group.withHeader(null).withFooter(null); //зануляем для сравнения, т.к на листе групп этих полей нет
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) ->g.getId()).max().getAsInt()))));
+
   }
 
 //  @Test (enabled = false)
