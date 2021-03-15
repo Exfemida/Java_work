ba45b7f5d54c77b9b3e52217348027f7f9888a4c
Exfemida <Exfemida@gmail.com>
2021-03-15 23:15:53 +0300

    готово редактирование контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -7,20 +7,21 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactDate;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupDate;
-
+import ru.stqa.pft.addressbook.model.Groups;
 import java.io.File;
-
 import static org.testng.Assert.assertEquals;
 
 public class ContactModificationTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
-    if (app.contact().all().size() == 0) {          //проверка наличия в базе созданных контактов
-      app.goTo().groupPage();                    //проверка наличия в базе созданных групп
-      if (app.group().all().size() == 0) {
+    if (app.db().contacts().size()==0) {
+      if (app.db().groups().size() == 0) {
+        app.goTo().groupPage();
         app.group().create(new GroupDate().withName("test2"));
       }
+      File photo = new File("src/test/resources/stru.png");
+      Groups groups =app.db().groups();
       app.goTo().AddContactPage();
       app.contact().create(new ContactDate()
               .withFirstname("Maria").withMiddlename("Sergeevna").withLastname("Ivanova")
@@ -29,7 +30,9 @@ public class ContactModificationTests extends TestBase {
               .withFax("5555555").withEmail("email_1").withEmail2("email_2").withEmail3("email_3")
               .withHomepage("mashka.ru").withBday("8").withBmonth("May").withByear("1982")
               .withAday("12").withAmonth("September").withAyear("2004")
-              .withNewGroup("test1").withAddress2("Kiev").withPhone2("34").withNotes("kak dela?"), true);
+              .withAddress2("Kiev").withPhone2("34").withNotes("kak dela?")
+              .withPhoto(photo)
+              .withNewGroup(groups.stream().map((g) ->g.getName()).findAny().get()), true);
     }
   }
 
