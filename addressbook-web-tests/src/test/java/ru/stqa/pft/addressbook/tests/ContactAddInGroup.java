99039852ed2641f13b1269d22d6780f0656ee0db
Exfemida <Exfemida@gmail.com>
2021-03-20 14:00:00 +0300

    нашли группу для добавления контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddInGroup.java
@@ -0,0 +1,38 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactDate;
+import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupDate;
+import ru.stqa.pft.addressbook.model.Groups;
+
+
+  public class ContactAddInGroup extends TestBase {
+
+    @Test
+    public void testAddContactInGroup() {
+      Contacts before = app.db().contacts();  //получили все контакты из базы
+      Groups groups = app.db().groups(); //получили все группы из базы
+      ContactDate modifyContact = before.iterator().next(); //выбрали контакт который будем добавлять в группу
+
+      System.out.println(modifyContact.getGroups());
+      int newGroupId;
+
+      //если группы из базы нет в списке групп контакта, то будем добавлять контакт в эту группу
+      for (GroupDate group: groups) {
+        boolean i=false;
+        for (GroupDate groupModifyContact : modifyContact.getGroups()) {
+          if (group.getId()==groupModifyContact.getId()){
+            i=true;
+          }
+        }
+        if (i==false) {
+          System.out.println(group.getId()+" berem");  //удалить
+          newGroupId=group.getId();
+        }
+      }
+    }
+  }
+
