abcba984c23238b0cb436ab469f1a8c891df0678
Exfemida <Exfemida@gmail.com>
2021-03-20 19:18:39 +0300

    добавление контакта в группу готово

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddInGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddInGroup.java
@@ -7,7 +7,35 @@ import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupDate;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.File;
+
 public class ContactAddInGroup extends TestBase {
+
+  @BeforeMethod
+  public void ensurePreconditions() {
+    if (app.db().contacts().size() == 0) {
+      if (app.db().groups().size() == 0) {
+        app.goTo().groupPage();
+        app.group().create(new GroupDate().withName("test2"));
+      }
+      File photo = new File("src/test/resources/stru.png");
+      Groups groups = app.db().groups();
+      app.goTo().AddContactPage();
+      app.contact().create(new ContactDate()
+              .withFirstname("Maria").withMiddlename("Sergeevna").withLastname("Ivanova")
+              .withNickname("Mashka").withTitle("do not know").withCompany("Rosneft")
+              .withAddress("Moskwa, 6").withHomePhone("2222222").withMobilePhone("3333333").withWorkPhone("4444444")
+              .withFax("5555555").withEmail("email_1").withEmail2("email_2").withEmail3("email_3")
+              .withHomepage("mashka.ru").withBday("8").withBmonth("May").withByear("1982")
+              .withAday("12").withAmonth("September").withAyear("2004")
+              .withAddress2("Kiev").withPhone2("34").withNotes("kak dela?")
+              .inGroup(groups.iterator().next())
+//                      .withNewGroup(groups.stream().map((g) -> g.getName()).findAny().get())
+              .withPhoto(photo), true);
+
+    }
+    app.goTo().StartPage();
+  }
 
   @Test
   public void testAddContactInGroup() throws Exception {
@@ -16,8 +44,9 @@ public class ContactAddInGroup extends TestBase {
     ContactDate oldcontact = modifyContact; //контакт до добавления группы
     System.out.println(modifyContact.getGroups());
 
-     GroupDate newGroupForAdd = new GroupDate();
+    GroupDate newGroupForAdd = new GroupDate();
     //если группы из базы нет в списке групп контакта, то будем добавлять контакт в эту группу
+    int flag = 0;
     while (newGroupForAdd.getName() == null) {
       Groups groups = app.db().groups(); //получили все группы из базы
       int i = 0;
@@ -38,19 +67,22 @@ public class ContactAddInGroup extends TestBase {
         app.goTo().groupPage();
         newGroupForAdd.withName("nametest 1").withHeader("headertest 1").withFooter("footertest 1");
         app.group().create(newGroupForAdd);
+        flag = 1;
       }
     }
 
-    //переименовываем группу для уникальности имени (использ при выборе)
-    Groups temp = app.db().groups();
-    int max = 0;
-    for (GroupDate g : temp) {
-      if (g.getId() > max) {
-        max = g.getId();
+    //переименовываем созданную группу для уникальности имени (использ при выборе)
+    if (flag==1) {
+      Groups temp = app.db().groups();
+      int max = 0;
+      for (GroupDate g : temp) {
+        if (g.getId() > max) {
+          max = g.getId();
+        }
       }
+      newGroupForAdd.withId(max).withName("nametest " + max).withHeader("headertest " + max).withFooter("footertest " + max);
+      app.group().modify(newGroupForAdd);
     }
-    newGroupForAdd.withId(max).withName("nametest " + max).withHeader("headertest " + max).withFooter("footertest " + max);
-    app.group().modify(newGroupForAdd);
 
     //добавляем контакт в группу
     app.goTo().StartPage();
