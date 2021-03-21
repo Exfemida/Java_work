6eb41a4d60c16cc42d7c27b011f88a9afc289f22
Exfemida <Exfemida@gmail.com>
2021-03-21 15:25:55 +0300

    продолжаем

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeleteFormGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeleteFormGroup.java
@@ -14,21 +14,31 @@ public class ContactDeleteFormGroup extends TestBase {
   @Test
   public void testDeleteContactFromGroup() throws Exception {
 
-    // Groups before=app.db().groups();
-    // GroupDate group = before.iterator().next();
+    GroupDate modGroup = new GroupDate();
+    ContactDate modContact = new ContactDate();
+    int modContactId;
+    int modGroupId = 0;
 
     Contacts contact = app.db().contacts();  //получили все контакты из базы
     Groups groups = new Groups();
-    GroupDate modGroup= new GroupDate();
+
     for (ContactDate cont : contact) {
       groups = cont.getGroups();
-      System.out.print(cont.getId());
-      System.out.println(groups);
+    //  System.out.print(cont.getId());
+    //  System.out.println(groups);
+      modContact = cont;
+    //  modContactId = cont.getId();
       if (cont.getGroups().size() != 0) {
         modGroup = cont.getGroups().iterator().next();
+        modGroupId = cont.getGroups().iterator().next().getId();
+    //    System.out.print(cont.getId());
+    //    System.out.println(groups);
       }
-
     }
-
+    if (modGroupId == 0) {
+      //добавить в последний контакт в любую группу
+      app.goTo().StartPage();
+      app.contact().addToGroup(modContact, app.db().groups().iterator().next());
+    }
   }
 }
