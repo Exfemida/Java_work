27dd8dad0cd655f7ddedccf476af989e91a7279d
Exfemida <Exfemida@gmail.com>
2021-03-27 18:42:26 +0300

    не та версия

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -9,7 +9,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import org.omg.CORBA.NameValuePair;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,8 +26,8 @@ public class HttpSession {
 
   public  boolean login(String username, String password) throws IOException{
     HttpPost post=new HttpPost (app.getProperty("web.baseUrl")+"/login.php");
-    List<NameValuePair> params=new ArrayList<NameValuePair>();
-    params.add(new BasicNameValuePair("username",username));
+    List<BasicNameValuePair> params=new ArrayList<>();
+    params.add(new BasicNameValuePair("username", username));
     params.add(new BasicNameValuePair("password",password));
     params.add(new BasicNameValuePair("secure_session","on"));
     params.add(new BasicNameValuePair("return","index.php"));
@@ -49,6 +49,7 @@ public class HttpSession {
     HttpGet get=new HttpGet(app.getProperty("web.baseUrl")+"/index.php");
     CloseableHttpResponse response=httpclient.execute(get);
     String body=geTextForm(response);
-    return body.contains(String.format("<span class=\"italic\">%s</span>",username));
+    //return body.contains(String.format("<span class=\"italic\">%s</span>",username));
+    return body.contains(String.format("<span class=\"label hidden-xs label-default arrowed\">%s</span>",username));
   }
 }
