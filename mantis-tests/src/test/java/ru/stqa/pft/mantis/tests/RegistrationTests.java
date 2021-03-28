b9000243b1923f038d759704b5930ff4536ff426
Exfemida <Exfemida@gmail.com>
2021-03-28 19:44:28 +0300

    до подключения библиотеки

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -3,6 +3,11 @@ package ru.stqa.pft.mantis.tests;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
 
 public class RegistrationTests extends TestBase{
 
@@ -11,10 +16,17 @@ public class RegistrationTests extends TestBase{
     app.mail().start();
   }
 
-  @Test
-  public void testRegistration(){
-    app.registration().start("user1", "user1.localhost.localdomain");
 
+  @Test
+  public void testRegistration() throws InterruptedException, IOException, MessagingException {
+    String email="user1.localhost.localdomain";
+    app.registration().start("user1", email);
+    List<MailMessage> mailMessages=app.mail().waitForMail(2, 10000);
+    findConfirmationLink(mailMessages,email);
+  }
+
+  private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+    MailMessage mailMessage=mailMessages.stream().filter((m)->m.to.equals(email)).findFirst().get();
   }
 
   @AfterMethod(alwaysRun = true)
