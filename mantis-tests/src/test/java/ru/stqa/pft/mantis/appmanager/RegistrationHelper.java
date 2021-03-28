8d142c0512e72e759838696c6b9ce7d12b5a8939
Exfemida <Exfemida@gmail.com>
2021-03-28 19:07:50 +0300

    не работает, но сохранить нужно

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,18 +1,17 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 
-public class RegistrationHelper {
+public class RegistrationHelper extends HelperBase{
 
-  private final ApplicationManager app;
-  private WebDriver wd;
-
-  public RegistrationHelper(ApplicationManager app) {
-    this.app=app;
-    wd=app.getDriver();
+   public RegistrationHelper (ApplicationManager app) {
+    super(app);
   }
 
   public void start(String userName, String email) {
     wd.get(app.getProperty("web.baseUrl")+"/signup_page.php");
+    type(By.name("username"), userName);
+    type(By.name("email"), email);
+    click(By.cssSelector("input[value='Signup']"));
   }
 }
