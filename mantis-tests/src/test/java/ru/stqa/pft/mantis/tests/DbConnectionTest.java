aa9aa26f19d8de0f8ae5d6bc19e05c77cd553b67
Exfemida <Exfemida@gmail.com>
2021-04-04 23:17:00 +0300

    сохранить

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/DbConnectionTest.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/DbConnectionTest.java
@@ -3,25 +3,27 @@ package ru.stqa.pft.mantis.tests;
 import org.testng.annotations.Test;
 import java.sql.*;
 
+import org.testng.annotations.Test;
+import java.sql.*;
+
 public class DbConnectionTest {
 
   @Test
   public void testDbConnection() {
     Connection conn = null;
     try {
-      conn = DriverManager.getConnection("jdbc:mysql://http://localhost:3306/addressbook?user=root&password="); //bugtracker
-    //  conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password="); //bugtracker
-  //    Statement st = conn.createStatement();
-  //    ResultSet rs = st.executeQuery("select id , username, email, password from mantis_user_table");
-     // Users users = new Users();
-     // while (rs.next()){
-     //   users.add (new UsersDate().withId(rs.getInt("id")).withUsername(rs.getString("username"))
-     //           .withEmail(rs.getString("email")).withPassword(rs.getString("password")));
-     // }
- //     rs.close();
- //     st.close();
-    //  conn.close();
- //     System.out.println(users);
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+      Statement st = conn.createStatement();
+//      ResultSet rs = st.executeQuery("select group_id , group_name, group_header, group_footer from group_list");
+ //     Groups groups = new Groups();
+ //     while (rs.next()){
+ //       groups.add (new GroupDate().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
+ //               .withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
+ //     }
+//      rs.close();
+//      st.close();
+ //     conn.close();
+ //     System.out.println(groups);
 
     } catch (SQLException ex) {
       // handle any errors
