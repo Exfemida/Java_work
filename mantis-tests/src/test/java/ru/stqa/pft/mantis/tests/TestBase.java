474a13e06f6df215fd5a1b1dee30f09437fe7417
Exfemida <Exfemida@gmail.com>
2021-04-22 23:20:45 +0300

    лог

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.mantis.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
@@ -24,6 +25,18 @@ public class TestBase {
   public void tearDown() throws Exception {
 //    app.ftp().restore("config_inc.php.bak", "config_inc.php");
     app.stop();
+  }
+
+  public boolean isIssueOpen(int issueId) {
+    boolean result = false; //по умолчанию не статус не close
+    //которая должна через Remote API получать из баг-трекера информацию о баг-репорте с заданным идентификатором, и возвращать значение false или true в зависимости от того, помечен он как исправленный или нет
+  return result;
+  }
+
+  public void skipIfNotFixed(int issueId) {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
   }
 
 
