ad1f41f1434c272f88e06b33697d13535110062d
Exfemida <Exfemida@gmail.com>
2021-06-05 14:32:16 +0300

    возвращен хром по умолчанию

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -21,7 +21,7 @@ import java.util.stream.Collectors;
 public class TestBase {
 
   Logger logger= LoggerFactory.getLogger(TestBase.class);
-  protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+  protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
