e792fd80ff8e125c6be193273b6f421d86710ecd
Exfemida <Exfemida@gmail.com>
2021-10-13 20:43:20 +0300

    тест выполняется
    Изменила кодировку проекта на 1251

--- Selenium/java-example/src/test/java/ru/stqa/training.selenium/MyFirstTest.java
+++ Selenium/java-example/src/test/java/ru/stqa/training.selenium/MyFirstTest.java
@@ -6,9 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
@@ -16,7 +14,7 @@ public class MyFirstTest {
   private WebDriver driver;
   private WebDriverWait wait;
 
-  @BeforeMethod
+  @BeforeTest
   public void start(){
     driver=new ChromeDriver();
     wait=new WebDriverWait(driver,10);
@@ -27,10 +25,11 @@ public class MyFirstTest {
     driver.get("http://www.google.com");
     driver.findElement(By.name("q")).sendKeys("webdriver");
     driver.findElement(By.name("btnK")).click();
-    wait.until(titleIs("webdriver - РџРѕРёСЃРє РІ Google"));
+    wait.until(titleIs("webdriver - Поиск в Google"));
   }
 
-  @AfterMethod
+  @AfterTest
+
   public  void stop(){
     driver.quit();
     driver=null;
